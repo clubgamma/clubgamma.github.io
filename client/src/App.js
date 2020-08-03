40ec499f4849da86513f4667849b5ca36daed648
chirayu-joshi <chirayujoshi1189@gmail.com>
2020-08-03 16:38:55 +0530

    Project initialised create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Club Gamma!</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
