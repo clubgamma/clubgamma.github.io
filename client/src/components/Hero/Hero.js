94f93335146264535b0b31b209e61208c0ade8aa
chirayu-joshi <chirayujoshi1189@gmail.com>
2020-08-12 12:06:31 +0530

    Implemented Dashboard component :raised_hands: | Fixed bugs for :iphone: & :computer:

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-// import styles from './Hero.module.css'
+import styles from './Hero.module.css'
+import ellipseImg from '../../assets/images/Ellipse.png'
+import fontPageSvg from '../../assets/images/Font_Page.svg'
+import maskImg from '../../assets/images/Mask_Group.png'
 
 const hero = props =>
-  <main style={{ paddingTop: '10vh', height: '100vh', width: '50px' }} id="home">
-    home
+  <main className={styles.dashboard} id="home">
+    <img src={ellipseImg} className={styles.ellipse1} alt="ellipse" />
+    <img src={fontPageSvg} className={styles.fontPage} alt="fontPage" />
+    <img src={ellipseImg} className={styles.ellipse2} alt="ellipse" />
+    <p className={styles.tagline}>
+      THE MORE <span>DIVERSITY</span> THE BETTER <span>COMMUNITY</span>
+    </p>
+    <img src={maskImg} className={styles.mask} alt="maskImg" />
+    <div className={styles.scrollDown}>
+      <div className={styles.mousey}>
+        <div className={styles.scroller}></div>
+      </div>
+    </div>
   </main>
 
 export default hero
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -41,35 +41,37 @@ class App extends Component {
     // Scroll Event
     window.addEventListener('scroll', this.handleScroll)
 
-    // Get all the section id and position, whose IDs are present in navbar
-    let sections = []
-    document.getElementById("navItemsContainer").childNodes.forEach(node => {
-      const section = document.getElementById(node.hash.slice(1, node.hash.length)).getBoundingClientRect()
-      sections.push({ id: node.hash, topPosition: section.top, bottomPosition: section.bottom })
-    })
-    this.setState({ sections: sections })
-
-    // Cursor animations
-    const cursor = document.getElementById('cursor')
-    const links = Array.prototype.slice.call(document.querySelectorAll('a'))
-    const headings = Array.prototype.slice.call(document.querySelectorAll('h1'))
-    const enlargeCursorOn = links.concat(headings)
-
-    document.addEventListener('mousemove', e => {
-      const { clientX, clientY } = e
-      cursor.style.left = clientX + 'px'
-      cursor.style.top = clientY + 'px'
-    })
-    enlargeCursorOn.forEach(element => {
-      element.addEventListener('mouseleave', () => {
-        cursor.style.width = '20px'
-        cursor.style.height = '20px'
+    if (window.screen.width >= 888) {
+      // Get all the section id and position, whose IDs are present in navbar
+      let sections = []
+      document.getElementById("navItemsContainer").childNodes.forEach(node => {
+        const section = document.getElementById(node.hash.slice(1, node.hash.length)).getBoundingClientRect()
+        sections.push({ id: node.hash, topPosition: section.top, bottomPosition: section.bottom })
       })
-      element.addEventListener('mouseover', () => {
-        cursor.style.width = '44px'
-        cursor.style.height = '44px'
+      this.setState({ sections: sections })
+
+      // Cursor animations
+      const cursor = document.getElementById('cursor')
+      const links = Array.prototype.slice.call(document.querySelectorAll('a'))
+      const headings = Array.prototype.slice.call(document.querySelectorAll('h1'))
+      const enlargeCursorOn = links.concat(headings)
+
+      document.addEventListener('mousemove', e => {
+        const { clientX, clientY } = e
+        cursor.style.left = clientX + 'px'
+        cursor.style.top = clientY + 'px'
       })
-    })
+      enlargeCursorOn.forEach(element => {
+        element.addEventListener('mouseleave', () => {
+          cursor.style.width = '20px'
+          cursor.style.height = '20px'
+        })
+        element.addEventListener('mouseover', () => {
+          cursor.style.width = '44px'
+          cursor.style.height = '44px'
+        })
+      })
+    }
   }
 
   render() {
@@ -80,7 +82,9 @@ class App extends Component {
 
     return (
       <Fragment>
-        <div className={styles.cursor} id="cursor"></div>
+        {window.screen.width >= 888
+          ? <div className={styles.cursor} id="cursor"></div>
+          : null}
         <Navbar
           scrolled={this.state.isScrolled}
           drawerClickHandler={this.drawerToggleHandler}
