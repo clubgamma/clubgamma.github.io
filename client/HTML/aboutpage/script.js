8dc20b3fa8e47b3d0be5b8722ce1856378a1574e
shubhamdraval <shubhamdraval@users.noreply.github.com>
2020-08-21 03:51:20 +0530

    Animation on scroll added to about us page :star2:

---
+++ client/HTML/aboutpage/script.js
@@ -0,0 +1,26 @@
+;(function() {
+    var throttle = function(type, name, obj) {
+        var obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+    throttle ("scroll", "optimizedScroll");
+})();
+
+var rOne = document.getElementById("fOne"),
+rTwo = document.getElementById("fTwo"),
+rThree = document.getElementById("fThree");
+    
+window.addEventListener("scroll", function() {
+    rOne.style.transform = "translate(50%,-50%) rotate("+window.pageYOffset+"deg)";
+    rTwo.style.transform = "translate(50%,-50%) rotate(-"+window.pageYOffset+"deg)";
+    rThree.style.transform = "translate(50%,-50%) rotate("+window.pageYOffset+"deg)";
+});
